c668ecf1abf580447b7546bc3bb26ea45d16d267
Arnah <github@arnah.ca>
2023-04-29 19:05:10 -0600

    fix prettier error

--- frontend/src/Components/Form/SeriesTypeSelectInput.tsx
+++ frontend/src/Components/Form/SeriesTypeSelectInput.tsx
@@ -37,7 +37,7 @@ const seriesTypeOptions: ISeriesTypeOption[] = [
     key: seriesTypes.EPISODE_TITLE,
     value: 'Episode Title',
     format: 'Episodes identified by the Episode Title',
-  }
+  },
 ];
 
 function SeriesTypeSelectInput(props: SeriesTypeSelectInputProps) {
